217634d1b0a7f226db255f5f5f44712b7c046b0b
cahan <cahannon538@naver.com>
2025-11-23 18:28:48 +0900

    디자인 변경 검색바

--- src/main/java/com/example/demo/scheduler/BoxOfficeScheduler.java
+++ src/main/java/com/example/demo/scheduler/BoxOfficeScheduler.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
+//@Component
 @RequiredArgsConstructor
 public class BoxOfficeScheduler {
     @Scheduled(cron = "0 0 3 * * *")
