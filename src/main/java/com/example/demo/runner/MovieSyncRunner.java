42f85f9330b0ba20ff9829957ea08eedfc50bebf
cahan <cahannon538@naver.com>
2025-11-30 16:57:58 +0900

    영화 초기화 추가

--- src/main/java/com/example/demo/runner/MovieSyncRunner.java
+++ src/main/java/com/example/demo/runner/MovieSyncRunner.java
@@ -1,6 +1,7 @@
 package com.example.demo.runner;
 
 import com.example.demo.external.adapter.KmdbAdapter;
+import com.example.demo.repository.MovieRepository;
 import com.example.demo.service.MovieSyncService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,19 +10,23 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 
-//@Component
+@Component
 @RequiredArgsConstructor
 @Slf4j
 @Profile("local")  // local 프로파일에서만 실행
 public class MovieSyncRunner implements CommandLineRunner {
     private final MovieSyncService movieSyncService;
-    private final KmdbAdapter kmdbAdapter;
-
+    private final MovieRepository movieRepository;
     @Override
     public void run(String... args) throws Exception{
-        System.out.println("🎬 KMDB 영화 동기화 시작...");
+        if (movieRepository.count() > 0) {
+            log.info("⏭ 기존 영화 데이터가 존재하여 초기 동기화를 건너뜁니다.");
+            return;
+        }
+
+        log.info("🎬 KMDB 영화 동기화 시작...");
         movieSyncService.syncMoviesByYear(2025);
-        System.out.println("✅ 동기화 완료!");
+        log.info("✅ 동기화 완료!");
         movieSyncService.syncMovieByTitle("가타카");
 
 
