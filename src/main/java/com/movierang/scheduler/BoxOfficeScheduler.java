aed8f2a3190e7615e776d662f1c81b656753ffef
cahan <cahannon538@naver.com>
2026-02-25 15:02:15 +0900

    스케줄러로 인한 KMDB 동기화와 KOBIS 캐시 삭제

--- src/main/java/com/movierang/scheduler/BoxOfficeScheduler.java
+++ src/main/java/com/movierang/scheduler/BoxOfficeScheduler.java
@@ -1,18 +1,29 @@
 package com.movierang.scheduler;
 
+import com.movierang.service.MovieSyncService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Slf4j
-//@Component
+@Component
 @RequiredArgsConstructor
 public class BoxOfficeScheduler {
+
+    private final MovieSyncService movieSyncService;
+
     @Scheduled(cron = "0 0 3 * * *")
-    @CacheEvict(value = "dailyBoxOffice")
-    public void evictDailyBoxOfficeCache() {
-        log.info("박스오피스 캐시 삭제 (스케줄)");
+    @CacheEvict(value = "dailyBoxOffice", allEntries = true)
+    public void dailyUpdate() {
+        int year = LocalDate.now().getYear();
+        log.info("일일 업데이트 시작 - 박스오피스 캐시 삭제, KMDB {}년 sync", year);
+
+        movieSyncService.syncMoviesByYear(year);
+
+        log.info("일일 업데이트 완료");
     }
 }
